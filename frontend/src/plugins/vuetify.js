8f336395cef4028dce744631b9eca95141c595d8
David Stark <david@starkers.org>
2019-08-15 12:58:35 +0100

    keep default styling

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,10 +4,12 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
-  theme: {
-    primary: '#9652ff',
-    success: '#3cd1c2',
-    info: '#ffaa2c',
-    error: '#f83e70'
-  }
+
+  // Modify default pallets a little here
+  // theme: {
+  //   primary: '#9652ff',
+  //   success: '#3cd1c2',
+  //   info: '#ffaa2c',
+  //   error: '#f83e70'
+  // }
 })
