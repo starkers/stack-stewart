d50b6e04f1303f231a69f794046045a0c9f2689f
David Stark <david@starkers.org>
2019-08-15 12:58:21 +0100

    un-used routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from './views/Dashboard.vue'
-import Projects from './views/Projects.vue'
-import Team from './views/Team.vue'
+// import Projects from './views/Projects.vue'
+// import Team from './views/Team.vue'
 
 Vue.use(Router)
 
@@ -15,16 +15,16 @@ export default new Router({
       name: 'dashboard',
       component: Dashboard
     },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: Projects
-    },
-    {
-      path: '/team',
-      name: 'team',
-      component: Team
-    }
+    // {
+    //   path: '/projects',
+    //   name: 'projects',
+    //   component: Projects
+    // },
+    // {
+    //   path: '/team',
+    //   name: 'team',
+    //   component: Team
+    // }
 
   ]
 })
